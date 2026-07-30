a2c424cc314aafaa0fbd42dcc91a221651a1bf64
rdsemon <codewithemons@gmail.com>
2026-07-30 21:33:38 +0600

    create UpdateFrom component

---
+++ app/components/UpdateFrom.tsx
@@ -0,0 +1,8 @@
+export default function UpdateFrom() {
+  return (
+    <form>
+      <input type="text" placeholder="enter your name" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
