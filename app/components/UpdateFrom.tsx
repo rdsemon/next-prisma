6fa989bfd173a24e99752662cecb769222772935
rdsemon <codewithemons@gmail.com>
2026-07-31 22:56:48 +0600

    use updateProfile server action and add new field input id

--- app/components/UpdateFrom.tsx
+++ app/components/UpdateFrom.tsx
@@ -1,7 +1,10 @@
-export default function UpdateFrom() {
+"use client";
+import { updateProfile } from "../lib/actions";
+export default function UpdateFrom({ id }: { id: string }) {
   return (
-    <form>
-      <input type="text" placeholder="enter your name" />
+    <form action={updateProfile}>
+      <input type="text" name="name" placeholder="enter your name" />
+      <input type="hidden" name="id" value={id} />
       <button type="submit">Submit</button>
     </form>
   );
